3683392c2d517b47b8063ec5ea31abd8af5d7315
Your Name <javiercardenastobon@gmail.com>
2021-09-21 19:59:10 -0500

    Entregables clase 6 todos los entregables estan compilados en este commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,5 @@
 const selector = document.getElementById("mainInput");
 
-// Clase Modeladora de Valores entrados por el usuario
 class Entry {
     constructor(value, date) {
         this.value = value;
@@ -11,17 +10,49 @@ class Entry {
     }
 
 }
-
-// Inicializo este objeto (valuesList) con arrays de multiples categorias (Por ahora solo Food)
 const valuesList = {
     food: [],
     rent: [],
     fun: []
 }
 
-const onSubmit = e => {
+const outcomesTotal = []
+
+const handleAdd = outcomesTotal => {
+    // Tarea 3 Esta funcion genera una operación basada en datos dados por el cliente se imprimen en el lateral
+    let added = 0;
+    for (let i = 0; i < outcomesTotal.length; i++) {
+        const element = outcomesTotal[i];
+        added += parseInt(element)
+    }
+    let add = document.getElementById('add')
+    add.textContent = added;
+}
+
+const handleOutcomes = entry => {
+    // Tarea 2 Esta funcion ordena las entradas de forma acsendente
+
+    outcomesTotal.push(entry);
+    outcomesTotal.sort()
+
+    console.log('Array de gastos ordenados', outcomesTotal)
+    handleAdd(outcomesTotal)
+}
+
+const handleNewEntry = value => {
+    // Tarea 1 Incorporar array a mi proyecto
     const date = new Date()
-    if (selector.value === "") {
+    const entry = new Entry(value, date);
+
+    valuesList.food.push(entry)
+
+    entry.report()
+}
+
+const onSubmit = e => {
+
+    const userInputValue = selector.value
+    if (userInputValue === "") {
         e.stopPropagation();
     } else {
         e.preventDefault();
@@ -29,18 +60,9 @@ const onSubmit = e => {
         const newLiItem = document.createElement("li");
         newLiItem.setAttribute('class','day_list-item');
         document.getElementById('ulToday').appendChild(newLiItem);
-        newLiItem.innerHTML = `<div class='day_list-item-category'><p class='day_list-item-category-icon'>🥙</p></div><span class='day_list-item-amount value'>${selector.value}</span>`;
-        
-        // Creo una nueva entrada
-        const entry1 = new Entry( selector.value, date);
-
-        // agrego cada entrada de usuario al array de su categoria (Por ahora solo Food)
-        valuesList.food.push(entry1)
-
-        // En la consola se pueden ver en la categoria de entradas Food y el reporte de creacion atraves método report()
-        console.log('Entradas a la categoría de Food', valuesList.food)
-        entry1.report()
-        
+        newLiItem.innerHTML = `<div class='day_list-item-category'><p class='day_list-item-category-icon'>🥙</p></div><span class='day_list-item-amount value'>${userInputValue}</span>`
+        handleNewEntry(userInputValue)
+        handleOutcomes(userInputValue)
         document.getElementById('mainForm').reset();
     }
 }
